4d6fbdf5419436b353eb486c0095562836a3b361
datumcoder <datumcoder@gmail.com>
2022-01-16 16:49:55 +0100

    updating gatsby config file for git hub pages to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
+    `pathPrefix: "/portfolio_2022"`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
